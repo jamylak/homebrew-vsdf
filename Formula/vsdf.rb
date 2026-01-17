8c8e13ca1eae56bb7d0fbcb7dc60da99d600ec85
jamylak <jamylak@gmail.com>
2026-01-18 00:32:59 +1100

    v0.2.5

--- Formula/vsdf.rb
+++ Formula/vsdf.rb
@@ -2,7 +2,7 @@ class Vsdf < Formula
   desc "Vulkan SDF Renderer + Video Recorder + Hot Reloader"
   homepage "https://github.com/jamylak/vsdf"
   # Replace this with the latest version tag from your release
-  version "0.2.0"
+  version "0.2.5"
   license "GPL-3.0-only"
 
   on_macos do
@@ -10,11 +10,11 @@ class Vsdf < Formula
     if Hardware::CPU.intel?
       url "https://github.com/jamylak/vsdf/releases/download/v#{version}/vsdf-macos-x86_64.tar.gz"
       # Replace this with the SHA256 checksum of the x86_64 macOS release tarball
-      sha256 "24afb799ec3e37034c45e98c652855680599337caec49a3f264a8abe97ea6cc9"
+      sha256 "6d2d8c45d75733453247fd284b7dc423e8643144d4f69c834a753e53c8c0a992"
     elsif Hardware::CPU.arm?
       url "https://github.com/jamylak/vsdf/releases/download/v#{version}/vsdf-macos-arm64.tar.gz"
       # Replace this with the SHA256 checksum of the arm64 macOS release tarball
-      sha256 "762f19bd7e47107e44acfde01d397d47d731ccc69de7e09278e833ab3b254ad4"
+      sha256 "638af8cf6262ae7c2dd1fc359795d24177e7bd3c7f3975061a9acc30cd51c4fe"
     end
   end
 
@@ -23,7 +23,7 @@ class Vsdf < Formula
     if Hardware::CPU.intel?
       url "https://github.com/jamylak/vsdf/releases/download/v#{version}/vsdf-linux-x86_64.tar.gz"
       # Replace this with the SHA256 checksum of the Linux release tarball
-      sha256 "5a02702234e3f7b4434e6089142c1f0b1daf0eb47607cecf728506ce5c1b591c"
+      sha256 "d220a405a68fe42f90abfd7807919533268de8e6248a4c5f9e3ee3c895838520"
     end
   end
 
